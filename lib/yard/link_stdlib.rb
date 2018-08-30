f40b6126bc8150d6e99dbba18caee897b2840472
nrser <neil@ztkae.com>
2018-08-30 22:17:55 +0800

    Some project and gem structure

---
+++ lib/yard/link_stdlib.rb
@@ -0,0 +1,7 @@
+require "yard/link_stdlib/version"
+
+module Yard
+  module LinkStdlib
+    # Your code goes here...
+  end
+end
---
+++ lib/yard/link_stdlib/version.rb
@@ -0,0 +1,5 @@
+module Yard
+  module LinkStdlib
+    VERSION = "0.1.0"
+  end
+end
